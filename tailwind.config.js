9fde7205ad453f84a6fe9c0505df9ccabde6617d
PhatJack <tienphatng.693@gmail.com>
2024-01-19 21:43:21 +0700

    feat:set up project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"desatured-dark-cyan": "hsl(180, 29%, 50%)",
+				"background-color": "hsl(180, 52%, 96%)",
+				"filter-tablets": "hsl(180, 31%, 95%)",
+				"dark-grayish-cyan": "hsl(180, 8%, 52%)",
+				"very-dark-grayish-cyan": "hsl(180, 14%, 20%)"
+			}
+		},
+	},
+	plugins: [],
+}
